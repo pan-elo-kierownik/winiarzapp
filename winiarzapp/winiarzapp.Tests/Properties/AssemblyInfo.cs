0237e252ada8b80033ca3c93c3437f9c6f0f93ed
Michał Wilski <michal.wilski@gmail.com>
2017-05-08 21:19:16 +0200

    Draft for test project.

---
+++ winiarzapp/winiarzapp.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("winiarzapp.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("winiarzapp.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("65cf1ee7-b7fe-4d02-947a-64b7dd00fc6a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ winiarzapp/winiarzapp.Tests/Tutorial.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+
+/*         
+    Test > Run > All tests
+*/
+
+namespace winiarzapp.Tests
+{
+    //TODO: Usunąć w końcowej wersji kodu.
+    [TestClass]
+    public class Tutorial
+    {
+        /// <summary>
+        /// Przykładowy test. Sprawdza czy 1 + 2 równa się 3. W ten sposób będą wyglądały wszystkie automatyczne testy.
+        /// </summary
+        [TestMethod]
+        public void Test1()
+        {
+            // Przygotuj
+            int a = 1;
+            int b = 2;
+
+            // Wykonaj
+            int result = a + b;
+
+            // Sprawdź
+            Assert.IsTrue(result == 3);
+        }
+
+        /// <summary>
+        /// Test sprawdzający, czy metoda rzuca wyjątek.
+        /// </summary
+        [TestMethod]
+        [ExpectedException(typeof(NotImplementedException))]
+        public void Test2()
+        {
+            throw new NotImplementedException();
+        }
+
+        //TODO: Testerzy, w tym projekcie (Winiarzapp.Tests) powinny pojawić się testy automatyczne. 
+
+    }
+}
