73ff66dbae9af2b11c0098fc07cd2c9c773343b5
marcinszulecki <marcinszulecki@gmail.com>
2017-06-07 20:22:31 +0200

    ListElement

--- winiarzapp/winiarzapp.UI/Windows/MainWindow/Components/ListElement.xaml.cs
+++ winiarzapp/winiarzapp.UI/Windows/MainWindow/Components/ListElement.xaml.cs
@@ -3,19 +3,30 @@ using Winiarzapp.Core.Data;
 
 namespace winiarzapp.UI.Windows.MainWindow.Components
 {
-    /// <summary>
-    /// Pojedynczy element listy. Powinien móc wyświetlać podstawowe informacje o przepisie (Nazwa oraz liczba zapisanych nastawień)
-    /// </summary>
+	/// <summary>
+	/// Pojedynczy element listy. Powinien móc wyświetlać podstawowe informacje o przepisie (Nazwa oraz liczba zapisanych nastawień)
+	/// </summary>
+	/// 
+	
     public partial class ListElement : UserControl
     {
-        public ListElement()
+
+		Recipe recipe;
+
+		public ListElement()
         {
             InitializeComponent();
         }
 
         public void RenderRecord(Recipe recipe)
         {
-            //TODO: Zaktualizuj wygląd elementu by odzwierciedlał rekord podany jako argument.
-        }
+			this.recipe = recipe;
+			
+			Label label = FindName("LabelListElement") as Label;
+
+			label.Content = recipe;
+		
+			//TODO: Zaktualizuj wygląd elementu by odzwierciedlał rekord podany jako argument.
+		}
     }
 }
