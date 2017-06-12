1c2b4218ccf31f8c576b172998f9d65158690a34
Michał Wilski <michal.wilski@gmail.com>
2017-06-12 22:56:51 +0200

    Fixed method arguments.

--- winiarzapp/winiarzapp.UI/Windows/MainWindow/Components/ListElement.xaml.cs
+++ winiarzapp/winiarzapp.UI/Windows/MainWindow/Components/ListElement.xaml.cs
@@ -3,35 +3,35 @@ using Winiarzapp.Core.Data;
 
 namespace winiarzapp.UI.Windows.MainWindow.Components
 {
-	/// <summary>
-	/// Pojedynczy element listy. Powinien móc wyświetlać podstawowe informacje o przepisie (Nazwa oraz liczba zapisanych nastawień)
-	/// </summary>
-	/// 
-	
+    /// <summary>
+    /// Pojedynczy element listy. Powinien móc wyświetlać podstawowe informacje o przepisie (Nazwa oraz liczba zapisanych nastawień)
+    /// </summary>
+    /// 
+
     public partial class ListElement : UserControl
     {
 
-		Recipe recipe;
+        Recipe recipe;
 
-		public ListElement()
+        public ListElement()
         {
             InitializeComponent();
         }
 
         public void RenderRecord(Recipe recipe)
         {
-			this.recipe = recipe;
-		
-			Label label1 = FindName("LabelListElement") as Label;
+            this.recipe = recipe;
 
-			label1.Content = recipe.Name;
-			
-			 //TODO: Zaktualizuj wygląd elementu by odzwierciedlał rekord podany jako argument.
-		}
-        
+            Label label1 = FindName("LabelListElement") as Label;
+
+            label1.Content = recipe.Name;
+
+            //TODO: Zaktualizuj wygląd elementu by odzwierciedlał rekord podany jako argument.
+        }
+
         private void LabelListElement_MouseDoubleClick(object sender, System.Windows.RoutedEventArgs e) //podwójne kliknięcie powodujące otwarcie Kreatora wina
         {
-            Winiarzapp.UI.Services.HelperService.ShowVineCreator(); //wywołanie metody wyświetlającej okienko z kreatorem wina
+            Winiarzapp.UI.Services.HelperService.ShowVineCreator(recipe); //wywołanie metody wyświetlającej okienko z kreatorem wina
         }
     }
 }
